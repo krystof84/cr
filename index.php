25c1a7e313b530fa2ce0f4b94b7427a1ead176a0
Krzysztof <krisczajka@gmail.com>
2019-04-12 10:09:19 +0200

    First commit

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Zadanie rekrutacyjne</title>
+    <meta name="description" content="">
+
+    <link rel="stylesheet" href="./src/css/main.css"/>
+</head>
+<body>
+
+
+<!-- DEV ONLY -->
+<script src="http://crafton.test:35729/livereload.js?snipver=1"></script>
+
+<script src="https://code.jquery.com/jquery-1.12.4.min.js"></script>
+<script src="./src/js/slick.min.js"></script>
+
+</body>
+</html>
